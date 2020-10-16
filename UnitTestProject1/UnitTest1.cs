cd083ece3f312ea72f5b178860be50bdcfb4facb
Jonas Heinrich <laiserfire@web.de>
2020-10-16 20:44:31 +0200

    Python installation location detection

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -5,6 +5,9 @@ using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Win32;
 
 namespace UnitTestProject1
 {
@@ -16,11 +19,71 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+
+        //get python path from environtment variable
+        string GetPythonPathfromPath()
+        {
+            var environmentVariables = Environment.GetEnvironmentVariables();
+            string pathVariable = environmentVariables["Path"] as string;
+            if (pathVariable != null)
+            {
+                string[] allPaths = pathVariable.Split(';');
+                foreach (var path in allPaths)
+                {
+                    string pythonPathFromEnv = path + "\\python.exe";
+                    if (File.Exists(pythonPathFromEnv))
+                        return pythonPathFromEnv;
+                }
+            }
+            return null;
+        }
+        string GetPythonPath()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu) + "\\Programs";
+            string localpath = Environment.GetFolderPath(Environment.SpecialFolder.StartMenu) + "\\Programs";
+
+            var files = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).ToList();
+            var localfiles = Directory.EnumerateFiles(localpath, "*", SearchOption.AllDirectories).ToList();
+            files.AddRange(localfiles);
+            var pythonFiles = files.Where(x => x.Contains("Python")).ToHashSet();
+            foreach (var potential in pythonFiles)
+            {
+                var directory = Path.GetDirectoryName(LinkResolver.ResolveShortcut(potential));
+                var pythonExe = Path.Combine(directory, "python.exe");
+                if (File.Exists(pythonExe))
+                {
+                    return pythonExe;
+                }
+
+            }
+            return null;
+        }
+
+
+        [TestMethod]
+        public void PythonPath()
+        {
+            var pythonPath = GetPythonPath();
+            Assert.IsNotNull(pythonPath);
+
+        }
+
+        [TestMethod]
+        public void TestResolve()
+        {
+            var resolved = LinkResolver.ResolveShortcut("C:\\Users\\laise\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Python 3.7\\IDLE (Python 3.7 64-bit).lnk");
+            Assert.IsNotNull(resolved);
+
+        }
+
         public void TestFunc()
         {
 
+            var pythonPath = GetPythonPath();
+            var pythonPathp = GetPythonPathfromPath();
+
             string pythonDir = @"C:\Users\laise\AppData\Local\Programs\Python\Python37";
-            string path = pythonDir + ";"+pythonDir+ "\\Scripts;" + Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
+            string path = pythonDir + ";" + pythonDir + "\\Scripts;" + Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Machine);
             Environment.SetEnvironmentVariable("PATH", path, EnvironmentVariableTarget.Process);
             //Environment.SetEnvironmentVariable("PYTHONHOME", @"c:\dev\libs\Anaconda3\envs\python2", EnvironmentVariableTarget.Process);
             //Environment.SetEnvironmentVariable("PYTHONPATH ", @"c:\dev\libs\Anaconda3\envs\python2\Lib", EnvironmentVariableTarget.Process);
@@ -53,21 +116,21 @@ namespace UnitTestProject1
                     scope.Exec("from dragonfly import *");
                     scope.Exec("path = os.getcwd()");
                     var s = scope.Get<string>("path");
-                    scope.Exec("with open (\""+fpath+"\", \"r\") as file:\n\texec(file.read())");
+                    scope.Exec("with open (\"" + fpath + "\", \"r\") as file:\n\texec(file.read())");
                     //var b = scope.Get<string>("bridge");
                     //scope.Exec(b);
                     //var tset = scope.Get("MyRule");
                     //scope.Exec("my_function(\"" + fpath + "\")");
-                    var v =scope.Get("MyRule");
+                    var v = scope.Get("MyRule");
                     var mem = v.GetDynamicMemberNames();
-                    var mapping =  v.GetAttr("mapping");
+                    var mapping = v.GetAttr("mapping");
                     var mem2 = mapping.GetDynamicMemberNames();
                     var items = mapping.GetAttr("keys");
                     var mem3 = items.GetDynamicMemberNames();
                     foreach (var item in mapping)
                     {
                         var target = mapping.GetItem(item.ToPython());
-                        Console.WriteLine(item+":"+ target);
+                        Console.WriteLine(item + ":" + target);
                         //target.InvokeMethod("execute");
                     }
                     var en = mapping.GetEnumerator();
