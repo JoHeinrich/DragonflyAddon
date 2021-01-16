b038e51a3a1dd418f93cbbfed6340b444145ce26
Jonas Heinrich <laiserfire@web.de>
2021-01-16 13:05:04 +0100

    Moved Check Files to Seperate Project

---
+++ VoiceCodingInstallationTools/Checks/PipPackages/CheckPipPackage3_8.cs
@@ -0,0 +1,11 @@
+﻿namespace DragonflyAddon
+{
+    public class CheckPipPackage3_8: CheckPipPackage
+    {
+        public CheckPipPackage3_8():base("numpy","dragonfly2","six","wheel","future","pywin32","wxPython==4.0.7")
+        {
+
+        }
+    }
+}
+
